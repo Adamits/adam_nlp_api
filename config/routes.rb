9ec2da666b1a78ef3e24a5da23e9703d58c9257c
Adam <ajwieme@gmail.com>
2016-03-14 14:33:12 -0500

    changing _ to - in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      post '/tf_idf/document/add', to: "tf_idf/document#add"
-      delete '/tf_idf/document/remove', to: "tf_idf/document#remove"
-      post '/tf_idf/document/term_scores', to: "tf_idf/document#update_term_scores"
-      get '/tf_idf/document/term_scores', to: "tf_idf/document#get_term_scores"
+      post '/tf-idf/document/add', to: "tf_idf/document#add"
+      delete '/tf-idf/document/remove', to: "tf_idf/document#remove"
+      post '/tf-idf/document/term-scores', to: "tf_idf/document#update_term_scores"
+      get '/tf-idf/document/term-scores', to: "tf_idf/document#get_term_scores"
     end
   end
 end
